d1f5a99ee4e75f0b86eae763936c498240e6ceef
Mohammad Issawi <2000issawi@gmail.com>
2020-10-31 03:52:32 +0200

    Created GLTF class header and structures

---
+++ include/Gltf.h
@@ -0,0 +1,51 @@
+namespace RDAGLTFParser{
+    class Gltf{
+private:
+        struct Attributes{
+            float* positionVectors,*normals,*UV_0_Coords;
+            int positionVectorsCount,normalsCount,UV_0_CoordsCount;
+            ~Attributes(){
+                delete[] positionVectors;
+                delete[] normals;
+                delete[] UV_0_Coords;
+            }
+        };
+        struct Primitvies
+        {
+            Attributes* attributes;
+            unsigned short* indices;
+            int indicesCount;
+            ~Primitvies(){
+                delete attributes;
+                delete[] indices;
+            }
+        };
+        struct Mesh
+        {
+            const char* name;
+            Primitvies* primitvies;
+            ~Mesh(){
+                delete primitvies;
+            }
+        };
+        struct Node{
+            const char * name;
+            Mesh* mesh;
+            ~Node(){
+                delete mesh;
+            }
+        };
+        struct Scene{
+            const char* name;
+            Node* nodes;
+            int nodesCount;
+            ~Scene(){
+                delete[] nodes;
+            }
+        };
+        Scene* scene;
+        char* readDataFromAccessor(int accessor,int& count);
+public:
+        Gltf(const char* path);
+    };
+}
